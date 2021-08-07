db64bb2b083c98b36b6f1cb3d97690c112b23e98
Christian Zamora <christianzamoraburgos@gmail.com>
2021-08-07 01:48:03 -0500

    added doctor table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_052934) do
+ActiveRecord::Schema.define(version: 2021_08_07_064729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "specialty"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
